80c801775a190ba32b8d34e8c73855bbe68aa4c9
Milan Lesichkov <lesichkovm@gmail.com>
2022-12-17 06:05:30 +0000

    Update SqlCreateTable.go

--- SqlCreateTable.go
+++ SqlCreateTable.go
@@ -4,14 +4,14 @@ package metastore
 func (st *Store) SqlCreateTable() string {
 	sqlMysql := `
 	CREATE TABLE IF NOT EXISTS ` + st.metaTableName + ` (
-	  ID varchar(40) NOT NULL PRIMARY KEY,
-	  ObjectType longtext NOT NULL,
-	  ObjectID longtext NOT NULL,
-	  Key longtext NOT NULL,
-	  Value longtext NOT NULL,
-	  CreatedAt datetime NOT NULL,
-	  UpdatedAt datetime,
-	  DeletedAt datetime
+	  id varchar(40) NOT NULL PRIMARY KEY,
+	  object_type longtext NOT NULL,
+	  object_id longtext NOT NULL,
+	  key longtext NOT NULL,
+	  value longtext NOT NULL,
+	  created_at datetime NOT NULL,
+	  updated_at datetime,
+	  deleted_at datetime
 	);
 	`
 
